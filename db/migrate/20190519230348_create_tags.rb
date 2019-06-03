710a029f8d09ebc7c345dfa424cf68df6e558457
jhonsd1996 <jasedanod@unal.edu.co>
2019-06-02 22:59:33 -0500

    updated docker-compose

--- db/migrate/20190519230348_create_tags.rb
+++ db/migrate/20190519230348_create_tags.rb
@@ -1,7 +1,7 @@
 class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
-      t.integer :passenger_id, null: false
+      t.string :passenger_id, null: false
       t.string :cabin_id, null: false
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+    Tag.create(
+        passenger_id: Faker::Blockchain::Bitcoin.address,
+        cabin_id: Faker::Blockchain::Bitcoin.address
+    )
+
+end
+
+
+1000.times do
+    Luggage.create(
+        weight: Faker::Number.between(1, 30),
+        status: Faker::Number.between(0, 2),
+        tag_id: Faker::Number.between(1, 100)
+    )
+end
